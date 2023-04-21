16238d5f877d52d51c58beb030a0c16e3b5e2fba
“NephatMutugi” <muchirinephat5@gmail.com>
2023-04-21 15:37:35 +0300

    Sorted squared arrays

---
+++ src/main/java/org/neph/arrays/SortSquaredArrays.java
@@ -0,0 +1,27 @@
+package org.neph.arrays;
+
+import java.util.Arrays;
+
+/**
+ * In this challenge, you are given an array as an input,
+ * square it and order the results in ascending order**/
+public class SortSquaredArrays {
+
+    public static void main(String[] args) {
+        int[] input = {-1, 10,0, 2, 12};
+        System.out.println("INPUT ARRAY: " + Arrays.toString(Arrays.stream(input).toArray()));
+        System.out.println("SORTED SQUARES:\n" + Arrays.toString(returnSortedSquares(input)));
+    }
+
+    private static int[] returnSortedSquares(int[] input){
+        int[] squaredArray = new int[input.length];
+        int i = 0;
+        for (int num : input){
+            squaredArray[i] = num * num;
+            i++;
+        }
+
+        Arrays.sort(squaredArray);
+        return squaredArray;
+    }
+}
