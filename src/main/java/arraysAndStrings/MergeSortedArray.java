2e083955491c80fc4fc02622202d9ce1b870bc34
“NephatMutugi” <muchirinephat5@gmail.com>
2023-04-27 11:27:56 +0300

    MergeSortedArray Optimisation

--- src/main/java/arraysAndStrings/MergeSortedArray.java
+++ src/main/java/arraysAndStrings/MergeSortedArray.java
@@ -1,35 +1,38 @@
 package arraysAndStrings;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author Nephat Muchiri
  * Date 25/04/2023
  */
 public class MergeSortedArray {
 
-    public static int[] mergeSortedArray(int[] arr1, int[] arr2){
+    public static List<Integer> mergeSortedArray(int[] arr1, int[] arr2){
         // Given to sorted arrays, merge them into a single array
 
-        // merged array len = arr1 + arr2
-        int[] mergedArr = new int[arr1.length + arr2.length];
+        List<Integer> mergedArr = new ArrayList<>();
         // initialize 3 counters, i for arr1, j for arr2, k for merged array
-        int i = 0, j = 0, k =0;
+        int i = 0, j = 0;
 
         while (i < arr1.length && j < arr2.length){
             if (arr1[i] < arr2[j]){
-                mergedArr[k] = arr1[i];
+                mergedArr.add(arr1[i]);
                 i++;
             } else {
-                mergedArr[k] = arr2[j];
+                mergedArr.add(arr2[j]);
                 j++;
             }
-            k++;
         }
 
         while (i < arr1.length){
-            mergedArr[k++] = arr1[i++];
+            mergedArr.add(arr1[i]);
+            i++;
         }
         while (j < arr2.length){
-            mergedArr[k++] = arr2[j++];
+            mergedArr.add(arr2[j]);
+            j++;
         }
 
         return mergedArr;
--- src/test/java/arraysAndStrings/MergeSortedArrayTest.java
+++ src/test/java/arraysAndStrings/MergeSortedArrayTest.java
@@ -1,35 +1,37 @@
 package arraysAndStrings;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.List;
 
 class MergeSortedArrayTest {
     @Test
     public void testMergeSortedArray(){
         int[] arr1 = {1, 3, 5, 7, 9};
         int[] arr2 = {2, 4, 6, 8, 10};
-        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        int[] result = MergeSortedArray.mergeSortedArray(arr1, arr2);
-        assertArrayEquals(expected, result);
+        List<Integer> result = MergeSortedArray.mergeSortedArray(arr1, arr2);
+        Assertions.assertEquals(expected, result);
 
         int[] arr3 = {1, 2, 3};
         int[] arr4 = {4, 5, 6, 7, 8, 9};
-        int[] expected2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int[] result2 = MergeSortedArray.mergeSortedArray(arr3, arr4);
-        assertArrayEquals(expected2, result2);
+        List<Integer> expected2 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        List<Integer>  result2 = MergeSortedArray.mergeSortedArray(arr3, arr4);
+        Assertions.assertEquals(expected2, result2);
 
         int[] arr5 = {1, 1, 1, 1, 1};
         int[] arr6 = {2, 2, 2, 2, 2};
-        int[] expected3 = {1, 1, 1, 1, 1, 2, 2, 2, 2, 2};
-        int[] result3 = MergeSortedArray.mergeSortedArray(arr5, arr6);
-        assertArrayEquals(expected3, result3);
+        List<Integer>  expected3 = Arrays.asList(1, 1, 1, 1, 1, 2, 2, 2, 2, 2);
+        List<Integer>  result3 = MergeSortedArray.mergeSortedArray(arr5, arr6);
+        Assertions.assertEquals(expected3, result3);
 
         int[] arr7 = {10, 20, 30};
         int[] arr8 = {40, 50, 60, 70};
-        int[] expected4 = {10, 20, 30, 40, 50, 60, 70};
-        int[] result4 = MergeSortedArray.mergeSortedArray(arr7, arr8);
-        assertArrayEquals(expected4, result4);
+        List<Integer>  expected4 = Arrays.asList(10, 20, 30, 40, 50, 60, 70);
+        List<Integer>  result4 = MergeSortedArray.mergeSortedArray(arr7, arr8);
+        Assertions.assertEquals(expected4, result4);
     }
 }
