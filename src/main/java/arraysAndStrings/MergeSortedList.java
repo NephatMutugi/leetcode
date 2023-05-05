f5c559be823131275f2702f81143054f9c5faa81
“NephatMutugi” <muchirinephat5@gmail.com>
2023-05-05 16:24:30 +0300

    Too lazy to insert a comment

---
+++ src/main/java/arraysAndStrings/MergeSortedList.java
@@ -0,0 +1,9 @@
+package arraysAndStrings;
+
+/**
+ * @Author Nephat Muchiri
+ * Date 02/05/2023
+ */
+public class MergeSortedList {
+    
+}
---
+++ src/test/java/arraysAndStrings/MergeSortedListTest.java
@@ -0,0 +1,7 @@
+package arraysAndStrings;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MergeSortedListTest {
+
+}
